c4daa453e58cfcb53647bfb6d9f47abca1e4868c
Sheryl Lee <sheryl_lee_wl@yahoo.com.sg>
2020-09-20 19:06:36 +1000

    Added comments

--- website/app.js
+++ website/app.js
@@ -6,22 +6,25 @@ let apiKey = '&units=metric&APPID=df36e04df26438b569e3497849b73d56';
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
+// Event listener for the element with the id: generate, with a callback function to execute when it is clicked.
 document.getElementById('generate').addEventListener('click', performAction);
 
-// GET request to the Open Weather info API
+// GET request to the Open Weather info API. This is a callback function to call your async GET request with the parameters: base url, user entered zip code (see input in html with id zip), personal API key
 function performAction(e){
+  // Assign variables to HTML Elements
   const newZip =  document.getElementById('zip').value;
   const feelings = document.getElementById('feelings').value;
-  getWeather(baseURL, newZip, apiKey)
+  getWeatherData(baseURL, newZip, apiKey)
   .then(function(data) {
-    postData('http://localhost:8000/addData', {date:newDate, temp:data.main.temp, userResponse:feelings})
+    postData('http://localhost:8000/addData', {date:d, temp:data.main.temp, userResponse:feelings})
     updateUI();
   })
   .then
     updateUI();
 }
 
-const getWeather = async (baseURL, newZip, apiKey)=>{
+// Async function that uses fetch() to make a GET request to the OpenWeatherMap API.
+const getWeatherData = async (baseURL, newZip, apiKey)=>{
 
   const res = await fetch(baseURL+newZip+apiKey)
   try {
@@ -32,32 +35,40 @@ const getWeather = async (baseURL, newZip, apiKey)=>{
   };
 };
 
+// Async function that uses fetch() to make a POST request to add the API data, as well as data entered by the user, to your app.
 const postData = async ( url = '', data = {})=>{
   const res = await fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    method: 'POST',
     credentials: 'same-origin',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(data) // body data type must match "Content-Type" header
+    body: JSON.stringify(data)
     });
 
   try {
+    // Convert to JSON
     const newData = await res.json();
     return newData;
+  // catches the error
   } catch(error) {
       console.log("error", error);
   };
 };
 
+// Updates the UI dynamically
 const updateUI = async ( url = '') => {
+  // Retrieve data from app
   const req = await fetch('/allData');
 
   try{
+    // Convert to JSON
     const allData = await req.json();
+    // Select elements and update the divs in HTML
     document.getElementById('date').innerHTML = allData[0].date;
     document.getElementById('temp').innerHTML = allData[0].temp;
     document.getElementById('content').innerHTML = allData[0].userResponse;
+  // catches the error
   } catch(error){
       console.log("error", error);
   }
